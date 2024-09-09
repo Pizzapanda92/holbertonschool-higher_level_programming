153640f5089df6148054495906f761133fb37b11
Pizzapanda92 <fabio92@sfr.fr>
2024-09-10 00:22:11 +0200

    exo 6 correction

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for args in ligne:
-            print("{:d}".format(args), end=" ")
+            if args < len(ligne) - 1:
+                print("{:d}".format(ligne[args]), end=" ")
+            else:
+                print("{:d}".format(args), end="")
         print()
