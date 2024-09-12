e50b08c39823102478bc91b88272b310d293dea3
Pizzapanda92 <fabio92@sfr.fr>
2024-09-12 14:39:35 +0200

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
+"""
+fuction that adds two numbers
+"""
+
+
 def add_integer(a, b=98):
+
+    """
+    Adds two integers or floats after casting them to integers.
+
+    Args:
+        a: The first integer or float.
+        b: The second integer or float, default is 98.
+
+    Returns:
+        The sum of a and b as an integer.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
+
     if not isinstance(a, (int, float)):
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError ("b must be an integer")
-    return int (a) + int(b)
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
