984de7b823e5e4167e2405104d3273d009a3f81c
Pizzapanda92 <fabio92@sfr.fr>
2024-09-17 16:33:30 +0200

    empty class Rectangle +README

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Currently, this class is empty and serves as a placeholder.
+    """
+    pass
