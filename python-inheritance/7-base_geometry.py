7da982b16b102597049f8a07d24a255de744dcc5
Pizzapanda92 <fabio92@sfr.fr>
2024-09-23 17:30:36 +0200

    exo 7: add raise typeerro and value erro

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A class BaseGeometry with an unimplemented area method"""
+
+
+class BaseGeometry:
+    """Represents a geometry object"""
+
+    def area(self):
+        """
+        Raises an exception to indicate the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates 'value' as an integer greater than 0.
+
+        Raises:
+        TypeError: If 'value' is not an integer.
+        ValueError: If 'value' is <= 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
