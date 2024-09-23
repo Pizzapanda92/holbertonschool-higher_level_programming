e1ff37d188a1f1674d684e6265176b42630aabd7
Pizzapanda92 <fabio92@sfr.fr>
2024-09-23 17:58:53 +0200

    exo 8: add def __init__(self, width, height): validated by integer_validator and private

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""A class BaseGeometry with an unimplemented area method"""
+
+
+class BaseGeometry:
+    """Represents a geometry object"""
+
+    def area(self):
+        """
+        Raises an exception to indicate the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates 'value' as an integer greater than 0.
+
+        Raises:
+        TypeError: If 'value' is not an integer.
+        ValueError: If 'value' is <= 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    class Rectangle:
+        """Represents a rectangle, inherits from BaseGeometry"""
+
+        def __init__(self, width, height):
+            """
+        Initializes a Rectangle with width and height.
+
+        Both width and height are validated as positive integers using
+        integer_validator method from BaseGeometry.
+
+        Attributes:
+        __width (int): The width of the rectangle (private).
+        __height (int): The height of the rectangle (private).
+        """
+
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+
+            self.__width = width
+            self.__height = height
