8054e2391107236ed337f3cef6cf684f41d525a8
Pizzapanda92 <fabio92@sfr.fr>
2024-09-23 16:33:17 +0200

    add comment exo 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,14 @@ or an instance of a class that inherited from the specified class.
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Check if obj is an instance of a_class or
+    a class that a_class inherits from.
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+    Returns:
+        True if obj is an instance or
+        inherits from a_class, otherwise False.
+    """
     return isinstance(obj, a_class)
