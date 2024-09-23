687ce18fffd5d687d1e0cacc6a8ded18c864d002
Pizzapanda92 <fabio92@sfr.fr>
2024-09-23 18:40:43 +0200

    correction bug ex9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -19,7 +19,7 @@ class BaseGeometry:
         TypeError: If 'value' is not an integer.
         ValueError: If 'value' is <= 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
