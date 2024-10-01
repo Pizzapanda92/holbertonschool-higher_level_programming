0dead1a40671c4610dd38de8abdb7eeb4440d806
Pizzapanda92 <fabio92@sfr.fr>
2024-10-01 16:04:17 +0200

    exo 3:function that returns the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+function that returns the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Write the Python object directly to the file in JSON format
+    """
+    return json.dumps(my_obj)
