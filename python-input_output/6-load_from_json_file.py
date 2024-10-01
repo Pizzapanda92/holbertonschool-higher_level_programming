ab6f99e726c0045b2257c94ddfadaabbec8249a7
Pizzapanda92 <fabio92@sfr.fr>
2024-10-01 17:07:25 +0200

    exo 6: function that creates an Object from a “JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that creates an object from a JSON file."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates and returns an object from a JSON file.
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f)
