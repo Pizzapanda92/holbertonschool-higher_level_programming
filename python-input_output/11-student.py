d0a7dfe39ae8f709c1ec7707ae1205410ad8e422
Pizzapanda92 <fabio92@sfr.fr>
2024-10-02 14:12:35 +0200

    correction incrementation

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -13,28 +13,26 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-
-def to_json(self, attrs=None):
-    """
+    def to_json(self, attrs=None):
+        """
     Retrieves a dictionary representation of a Student instance.
     If attrs is a list of strings, only those attributes are retrieved.
     Otherwise, all attributes are retrieved.
     """
-    if isinstance(attrs, list):
-        result = {}
-        for attr in attrs:
-            if attr in self.__dict__:
-                result[attr] = self.__dict__[attr]
-        return result
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+            return result
 
-    else:
-        return self.__dict__
+        else:
+            return self.__dict__
 
-
-def reload_from_json(self, json):
-    """
-    Replaces all attributes of the Student instance based on the JSON dictionary.
-    """
-    for key in json:
-        if key in self.__dict__:
-            self.__dict__[key] = json[key]
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance based on the JSON dictionary.
+        """
+        for key in json:
+            if key in self.__dict__:
+                self.__dict__[key] = json[key]
