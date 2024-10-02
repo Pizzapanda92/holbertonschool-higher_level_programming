52ca4aa2b793d1b5981c10713b688d2d725cd450
Pizzapanda92 <fabio92@sfr.fr>
2024-10-02 13:15:17 +0200

    add class Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,17 +4,17 @@ class Student that defines a student
 """
 
 
-def __init__(self, first_name, last_name, age):
-    """
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
         Initialize the student with first name, last name, and age.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-
-def to_json(self):
-    """
-        Return a dictionary representation of the instance.
-    """
-    return self.__dict__
+    def to_json(self):
+        """
+            Return a dictionary representation of the instance.
+        """
+        return self.__dict__
