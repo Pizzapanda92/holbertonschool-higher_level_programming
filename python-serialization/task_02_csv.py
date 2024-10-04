a7555ddc3b307dd1bef55fcee3bb6a34f831b66f
Pizzapanda92 <fabio92@sfr.fr>
2024-10-04 11:55:43 +0200

    exo 2 :convert csv to Json

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import json
+import csv
+
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, 'r',) as Nfile:
+            csv_data = csv.DictReader(Nfile)
+            data_list = list(csv_data)
+
+        with open('data.json', 'w') as j_file:
+            json.dump(data_list, j_file, indent=4)
+        return True
+    except FileNotFoundError:
+        return False
+    except:
+        return False
