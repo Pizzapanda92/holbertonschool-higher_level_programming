ce015fd1dad9f19061184f62ea8493ef4e8b9a9f
Pizzapanda92 <fabio92@sfr.fr>
2024-10-14 15:08:47 +0200

    add data route

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -12,6 +12,11 @@ def home():
     return ('Welcome to the Flask API!')
 
 
+@app.route('/data')
+def data():
+    return jsonify(list(users.keys()))
+
+
 @app.route("/status")
 def statu():
     return ("ok")
@@ -27,15 +32,17 @@ def get_user(username):
 
 @app.route("/add_user", methods=["POST"])
 def add_users():
-    if not request.json or "username" not in request.json:
+    data = request.get_json()
+
+    if "username" not in data:
         return jsonify({"error": "Username is required"}), 400
 
-    username = request.json["username"]
+    username = data["username"]
     users[username] = {
         "username": username,
-        "name": request.json.get("name", ""),
-        "age": request.json.get("age", 0),
-        "city": request.json.get("city", "")
+        "name": data.get("name", ""),
+        "age": data.get("age", 0),
+        "city": data.get("city", "")
     }
 
     return jsonify({"message": "User added", "user": users[username]}), 201
