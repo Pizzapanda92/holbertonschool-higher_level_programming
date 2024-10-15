9cc3446654a2f1ab36567bcc9b8f1c05c0d6e106
Pizzapanda92 <fabio92@sfr.fr>
2024-10-15 19:39:15 +0200

    correction users to user (l24)

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -21,7 +21,7 @@ users = {
 @auth.verify_password
 def verify_password(username, password):
     user = users.get(username)
-    if user and check_password_hash(users["password"], password):
+    if user and check_password_hash(user["password"], password):
         return user
 
 
