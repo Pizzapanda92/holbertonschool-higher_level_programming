ec1e908901ed69604eb27fe295bd1f2a05f403f0
Pizzapanda92 <fabio92@sfr.fr>
2024-10-15 19:07:20 +0200

    change password

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -13,8 +13,8 @@ app.config['JWT_SECRET_KEY'] = 'your_secret_key'
 jwt = JWTManager(app)
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password1"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password2"), "role": "admin"}
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
 
