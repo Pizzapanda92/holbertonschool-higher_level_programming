6a86d5921fa74682ae0e690e5d6bcb95a94e529a
Pizzapanda92 <fabio92@sfr.fr>
2024-10-30 23:28:13 +0100

    correction exo 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists all TV shows with their genre, displaying NULL for shows without a genre, sorted by show title and genre name
 SELECT tv_shows.title,
-    tv_genres.name AS genre
+    tv_genres.name
 FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC,
-    genre ASC;
+    tv_genres.name ASC;
