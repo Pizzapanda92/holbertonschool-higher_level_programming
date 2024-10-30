f9d1ba1cc0e8057c17c9b684a6ac787af0e2c4e8
Pizzapanda92 <fabio92@sfr.fr>
2024-10-30 18:32:39 +0100

    correction v_show to tv_show

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -5,4 +5,4 @@ FROM tv_shows
     LEFT JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC,
-    v_show_genres.genre_id ASC;
+    tv_show_genres.genre_id ASC;
