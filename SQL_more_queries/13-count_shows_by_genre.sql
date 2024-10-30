c140607d40395d59fa240108c81581d0edfb584c
Pizzapanda92 <fabio92@sfr.fr>
2024-10-30 22:25:59 +0100

    Lists all genres with the number of shows linked

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres with the number of shows linked to each, sorted by number of shows in descending order
+SELECT tv_genres.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+    JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY tv_show_genres.genre_id ASC;
