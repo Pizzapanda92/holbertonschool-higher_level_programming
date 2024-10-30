79e659ff4bf3d5f1bce550a48b2654d3675759f6
Pizzapanda92 <fabio92@sfr.fr>
2024-10-30 23:26:02 +0100

    Lists all TV shows with their genre

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- Lists all TV shows with their genre name, including shows without a genre, sorted by show title and genre name
+-- Lists all TV shows with their genre, displaying NULL for shows without a genre, sorted by show title and genre name
 SELECT tv_shows.title,
-    COALESCE(tv_genres.name, 'NULL') AS genre
+    tv_genres.name AS genre
 FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
