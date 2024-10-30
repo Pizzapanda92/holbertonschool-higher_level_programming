341883e3350c38daa1170ac8772d754de4481ce7
Pizzapanda92 <fabio92@sfr.fr>
2024-10-30 18:24:41 +0100

    Lists all TV shows without an associated genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all TV shows without an associated genre, sorted by show title in ascending order
+SELECT tv_shows.title,
+    tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC,
