a527c50d891b8f9eccd22146b522deaa06c662b0
Pizzapanda92 <fabio92@sfr.fr>
2024-11-01 12:22:36 +0100

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    search_value = sys.argv[4]
+    cursor.execute(
+        "SELECT * frome states WHERE name IN (sys.argv[4]) ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
