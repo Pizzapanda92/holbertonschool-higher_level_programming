313de01c8e690975ce8c7c12263c6956b257451d
Pizzapanda92 <fabio92@sfr.fr>
2024-11-01 12:47:20 +0100

    correction pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""cript that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
 
-    # Connect to MySQL database using command-line arguments: user, password, database name
+    """Connect to MySQL database using command-line arguments:
+        user, password, database name"""
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
