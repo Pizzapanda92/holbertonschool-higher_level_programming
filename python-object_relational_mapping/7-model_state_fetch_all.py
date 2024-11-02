75796aba205f5c2fecdbca8a3a77a60aa702232c
Pizzapanda92 <fabio92@sfr.fr>
2024-11-02 17:34:31 +0100

    modif create_engine

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,7 @@
 """
 Lists all State objects from the database hbtn_0e_6_usa, sorted by ascending id.
 """
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     # Establish a connection to the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        mysql_username, mysql_password, database_name), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
