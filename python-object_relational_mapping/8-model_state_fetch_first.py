a0d5b8a5ca9a3302e57ab965316aafbe2e28cbb0
Pizzapanda92 <fabio92@sfr.fr>
2024-11-03 15:27:14 +0100

    test correction bug

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,12 +12,14 @@ import sys
 
 if __name__ == '__main__':
 
+    # Connect to the database
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        mysql_username, mysql_password, database_name))
+        mysql_username, mysql_password, database_name), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
