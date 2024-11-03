717f9c9f1adde999717b2342be9898c739ab65bb
Pizzapanda92 <fabio92@sfr.fr>
2024-11-03 15:21:00 +0100

    correction bug

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first(1)
+    state = session.query(State).order_by(State.id).first()
 
     if state:
         print("{}: {}".format(state.id, state.name))
