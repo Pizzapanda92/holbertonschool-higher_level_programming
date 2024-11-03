9412c7d1c406b939394d7076dd647b248eff48ea
Pizzapanda92 <fabio92@sfr.fr>
2024-11-03 19:53:11 +0100

    change name when id = 2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Updates the name of the State object with id = 2 to "New Mexico"
+in the database hbtn_0e_6_usa.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        mysql_username, mysql_password, database_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+
+    if state:
+        state.name = "New Mexico"
+
+    session.commit()
+    session.close()
