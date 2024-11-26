ff5515ee81b53c8b617181b3385aceb4cf148381
Pizzapanda92 <fabio92@sfr.fr>
2024-11-26 19:45:47 +0100

    add stylr red to header

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+let redHeader = document.querySelector("#red_header");
+let header = document.querySelector("header");
+redHeader.addEventListener("click", function () {
+  header.style.color = '#FF0000';
+});
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+let redHeader = document.querySelector("#red_header");
+let header = document.querySelector("header");
+
+redHeader.addEventListener("click", function () {
+    header.classList.add("red");
+});
